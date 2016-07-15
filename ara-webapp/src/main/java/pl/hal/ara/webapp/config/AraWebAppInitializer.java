442a34bd451d07cc92692228cc2e9dac454ac49e
HAL <phep45@gmail.com>
2016-07-15 22:59:06 +0200

    boot

---
+++ ara-webapp/src/main/java/pl/hal/ara/webapp/config/AraWebAppInitializer.java
@@ -0,0 +1,12 @@
+package pl.hal.ara.webapp.config;
+
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletException;
+
+public class AraWebAppInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(javax.servlet.ServletContext servletContext) throws ServletException {
+
+    }
+}
